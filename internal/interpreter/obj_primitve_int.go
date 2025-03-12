48c6423a146e7bf35905b9312811aedf17b13acb
squizzling <22306301+squizzling@users.noreply.github.com>
2025-03-12 10:19:43 -0700

    interp: ObjectInt should try to use reverse operations if available

--- internal/interpreter/obj_primitve_int.go
+++ internal/interpreter/obj_primitve_int.go
@@ -10,10 +10,20 @@ type ObjectInt struct {
 	Value int
 }
 
+type methodIntOp struct {
+	Object
+
+	op      string
+	reverse string
+}
+
 func NewObjectInt(i int) Object {
 	return &ObjectInt{
 		Object: NewObject(map[string]Object{
-			"__add__": methodIntAdd{NewObject(nil)},
+			"__add__":     methodIntOp{Object: NewObject(nil), op: "+", reverse: "__radd__"},
+			"__mul__":     methodIntOp{Object: NewObject(nil), op: "*", reverse: "__rmul__"},
+			"__sub__":     methodIntOp{Object: NewObject(nil), op: "-", reverse: "__rsub__"},
+			"__truediv__": methodIntOp{Object: NewObject(nil), op: "/", reverse: "__rtruediv__"},
 		}),
 		Value: i,
 	}
@@ -23,15 +33,21 @@ func (oi *ObjectInt) Repr() string {
 	return fmt.Sprintf("int(%d)", oi.Value)
 }
 
-type methodIntAdd struct {
-	Object
-}
-
-func (mia methodIntAdd) Params(i *Interpreter) (*Params, error) {
+func (mio methodIntOp) Params(i *Interpreter) (*Params, error) {
 	return BinaryParams, nil
 }
 
-func (mia methodIntAdd) Call(i *Interpreter) (Object, error) {
+func (mio methodIntOp) Call(i *Interpreter) (Object, error) {
+	if right, err := i.Scope.GetArg("right"); err != nil {
+		return nil, err
+	} else if reverseOp, err := right.Member(i, right, mio.reverse); err == nil {
+		// If it exists, we always use the reverse method, because it's more likely to be the intended behavior.
+		// We explicitly don't expose reverse methods for primitives though.
+		if reverseOpCall, ok := reverseOp.(FlowCall); ok {
+			return reverseOpCall.Call(i)
+		}
+	}
+
 	if self, err := ArgAs[*ObjectInt](i, "self"); err != nil {
 		return nil, err
 	} else if right, err := i.Scope.GetArg("right"); err != nil {
@@ -39,11 +55,22 @@ func (mia methodIntAdd) Call(i *Interpreter) (Object, error) {
 	} else {
 		switch right := right.(type) {
 		case *ObjectInt:
-			return NewObjectInt(self.Value + right.Value), nil
+			switch mio.op {
+			case "+":
+				return NewObjectInt(self.Value + right.Value), nil
+			case "-":
+				return NewObjectInt(self.Value - right.Value), nil
+			case "/":
+				return NewObjectInt(self.Value / right.Value), nil
+			case "*":
+				return NewObjectInt(self.Value * right.Value), nil
+			default:
+				return nil, fmt.Errorf("methodIntOp: unknown op %s", mio.op)
+			}
 		default:
-			return nil, fmt.Errorf("methodIntAdd: unknown type %T", right)
+			return nil, fmt.Errorf("methodIntOp: unknown type %T", right)
 		}
 	}
 }
 
-var _ = FlowCall(methodIntAdd{})
+var _ = FlowCall(methodIntOp{})
