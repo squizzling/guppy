e60554a9e46da1572e790d27de39a554cf6c05b4
squizzling <22306301+squizzling@users.noreply.github.com>
2025-03-12 12:51:42 -0700

    flow: add filter binary or

--- internal/flow/filter/interface.go
+++ internal/flow/filter/interface.go
@@ -13,9 +13,11 @@ type Filter interface {
 func newFilterObject() interpreter.Object {
 	return interpreter.NewObject(map[string]interpreter.Object{
 		"__binary_and__":       methodBinaryAnd{},
+		"__binary_or__":        methodBinaryOr{},
 		"__unary_binary_not__": methodBinaryNot{},
 	})
 }
 
 var _ = interpreter.FlowCall(methodBinaryAnd{})
+var _ = interpreter.FlowCall(methodBinaryOr{})
 var _ = interpreter.FlowCall(methodBinaryNot{})
