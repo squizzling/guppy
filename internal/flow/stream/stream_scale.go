a5b175311f3f91db5db5dd86b59e7c37135cfa21
squizzling <22306301+squizzling@users.noreply.github.com>
2025-03-24 16:08:30 -0700

    flow: make methodScale take a float64

--- internal/flow/stream/stream_scale.go
+++ internal/flow/stream/stream_scale.go
@@ -19,12 +19,14 @@ func (ms methodScale) Params(i *interpreter.Interpreter) (*interpreter.Params, e
 	}, nil
 }
 
-func resolveMultiple(i *interpreter.Interpreter) (int, error) {
+func (ms methodScale) resolveMultiple(i *interpreter.Interpreter) (float64, error) {
 	if multiple, err := i.Scope.GetArg("multiple"); err != nil {
 		return 0, err
 	} else {
 		switch multiple := multiple.(type) {
 		case *interpreter.ObjectInt:
+			return float64(multiple.Value), nil
+		case *interpreter.ObjectDouble:
 			return multiple.Value, nil
 		default:
 			return 0, fmt.Errorf("duration is %T not *interpreter.ObjectInt", multiple)
@@ -35,7 +37,7 @@ func resolveMultiple(i *interpreter.Interpreter) (int, error) {
 func (ms methodScale) Call(i *interpreter.Interpreter) (interpreter.Object, error) {
 	if self, err := interpreter.ArgAs[Stream](i, "self"); err != nil {
 		return nil, err
-	} else if multiple, err := resolveMultiple(i); err != nil {
+	} else if multiple, err := ms.resolveMultiple(i); err != nil {
 		return nil, err
 	} else {
 		return NewScale(self, multiple), nil
@@ -46,10 +48,10 @@ type scale struct {
 	interpreter.Object
 
 	source   Stream
-	multiple int
+	multiple float64
 }
 
-func NewScale(source Stream, multiple int) Stream {
+func NewScale(source Stream, multiple float64) Stream {
 	return &scale{
 		Object:   newStreamObject(),
 		source:   unpublish(source),
@@ -58,5 +60,5 @@ func NewScale(source Stream, multiple int) Stream {
 }
 
 func (s *scale) RenderStream() string {
-	return fmt.Sprintf("%s.scale(%d)", s.source.RenderStream(), s.multiple)
+	return fmt.Sprintf("%s.scale(%f)", s.source.RenderStream(), s.multiple)
 }
