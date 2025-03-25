5b081eac14fbae0dc17cebc360ed49d2cbc391b7
squizzling <22306301+squizzling@users.noreply.github.com>
2025-03-25 13:27:35 -0700

    interp: implement list + list

--- internal/interpreter/obj_primitve_list.go
+++ internal/interpreter/obj_primitve_list.go
@@ -1,6 +1,7 @@
 package interpreter
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -13,8 +14,10 @@ type ObjectList struct {
 
 func NewObjectList(items ...Object) Object {
 	return &ObjectList{
-		Object: NewObject(nil),
-		Items:  items,
+		Object: NewObject(map[string]Object{
+			"__add__": methodListAdd{Object: NewObject(nil)},
+		}),
+		Items: items,
 	}
 }
 
@@ -29,4 +32,30 @@ func (ol *ObjectList) Repr() string {
 	}
 	sb.WriteString(")")
 	return sb.String()
+}
+
+type methodListAdd struct {
+	Object
+}
+
+func (mla methodListAdd) Params(i *Interpreter) (*Params, error) {
+	return BinaryParams, nil
+}
+
+func (mla methodListAdd) Call(i *Interpreter) (Object, error) {
+	if self, err := ArgAs[*ObjectList](i, "self"); err != nil {
+		return nil, err
+	} else if right, err := i.Scope.GetArg("right"); err != nil {
+		return nil, err
+	} else {
+		switch right := right.(type) {
+		case *ObjectList:
+			items := make([]Object, 0, len(self.Items)+len(right.Items))
+			items = append(items, self.Items...)
+			items = append(items, right.Items...)
+			return NewObjectList(items...), nil
+		default:
+			return nil, fmt.Errorf("methodListAdd: unknown type %T", right)
+		}
+	}
 }
