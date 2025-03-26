f317a5a771fcf01d65c7def7ae70d3101be4fed4
squizzling <22306301+squizzling@users.noreply.github.com>
2025-03-26 13:39:30 -0700

    interp: implement __eq__ and __ne__ for None

--- internal/interpreter/obj_none.go
+++ internal/interpreter/obj_none.go
@@ -1,19 +1,60 @@
 package interpreter
 
-import (
-	"errors"
-)
-
-type ObjectNone struct{}
+type ObjectNone struct {
+	Object
+}
 
 func NewObjectNone() Object {
-	return &ObjectNone{}
+	return &ObjectNone{
+		NewObject(map[string]Object{
+			"__eq__": methodNoneEqual{Object: NewObject(nil)},
+			"__ne__": methodNoneNotEqual{Object: NewObject(nil)},
+		}),
+	}
 }
 
 func (on *ObjectNone) Repr() string {
 	return "None"
 }
 
-func (on *ObjectNone) Member(i *Interpreter, obj Object, memberName string) (Object, error) {
-	return nil, errors.New("None doesn't support member")
+type methodNoneEqual struct {
+	Object
+}
+
+func (mne methodNoneEqual) Params(i *Interpreter) (*Params, error) {
+	return BinaryParams, nil
+}
+
+func (mne methodNoneEqual) Call(i *Interpreter) (Object, error) {
+	if right, err := i.Scope.GetArg("right"); err != nil {
+		return nil, err
+	} else {
+		switch right.(type) {
+		case *ObjectNone:
+			return NewObjectBool(true), nil
+		default:
+			return NewObjectBool(false), nil
+		}
+	}
+}
+
+type methodNoneNotEqual struct {
+	Object
+}
+
+func (mnne methodNoneNotEqual) Params(i *Interpreter) (*Params, error) {
+	return BinaryParams, nil
+}
+
+func (mne methodNoneNotEqual) Call(i *Interpreter) (Object, error) {
+	if right, err := i.Scope.GetArg("right"); err != nil {
+		return nil, err
+	} else {
+		switch right.(type) {
+		case *ObjectNone:
+			return NewObjectBool(false), nil
+		default:
+			return NewObjectBool(true), nil
+		}
+	}
 }
