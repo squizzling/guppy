3cc023552a5a613252902e601fbf331fdeb1141b
squizzling <22306301+squizzling@users.noreply.github.com>
2025-10-06 13:14:09 -0700

    interp: re-add bool ternary

--- pkg/interpreter/obj_primitve_bool.go
+++ pkg/interpreter/obj_primitve_bool.go
@@ -1,5 +1,9 @@
 package interpreter
 
+import (
+	"guppy/pkg/parser/ast"
+)
+
 type ObjectBool struct {
 	Object
 
@@ -10,5 +14,13 @@ func NewObjectBool(v bool) Object {
 	return &ObjectBool{
 		Object: NewObject(nil),
 		Value:  v,
+	}
+}
+
+func (ob *ObjectBool) VisitExpressionTernary(i *Interpreter, left ast.Expression, right ast.Expression) (any, error) {
+	if ob.Value {
+		return left.Accept(i)
+	} else {
+		return right.Accept(i)
 	}
 }
