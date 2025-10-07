9147c27bb127830e56bad1116caf4d7d2b7be6b3
squizzling <22306301+squizzling@users.noreply.github.com>
2025-10-07 13:55:30 -0700

    interp: add bool.__unary_binary_not__

--- pkg/interpreter/obj_primitve_bool.go
+++ pkg/interpreter/obj_primitve_bool.go
@@ -12,8 +12,10 @@ type ObjectBool struct {
 
 func NewObjectBool(v bool) Object {
 	return &ObjectBool{
-		Object: NewObject(nil),
-		Value:  v,
+		Object: NewObject(map[string]Object{
+			"__unary_binary_not__": methodBoolUnaryBinaryNot{Object: NewObject(nil)},
+		}),
+		Value: v,
 	}
 }
 
@@ -26,3 +28,19 @@ func (ob *ObjectBool) VisitExpressionTernary(i *Interpreter, left ast.Expression
 }
 
 var _ FlowTernary = (*ObjectBool)(nil)
+
+type methodBoolUnaryBinaryNot struct {
+	Object
+}
+
+func (mbubn methodBoolUnaryBinaryNot) Params(i *Interpreter) (*Params, error) {
+	return UnaryParams, nil
+}
+
+func (mbubn methodBoolUnaryBinaryNot) Call(i *Interpreter) (Object, error) {
+	if selfValue, err := ArgAsBool(i, "self"); err != nil {
+		return nil, err
+	} else {
+		return NewObjectBool(!selfValue), nil
+	}
+}
