caa9988f0185b023c3d06ecf5ce8d744a6d1fea0
squizzling <22306301+squizzling@users.noreply.github.com>
2025-11-14 18:54:15 -0500

    interp: Add tests for ObjectList and ObjectTuple

---
+++ pkg/interpreter/primitive/obj_primitive_list_test.go
@@ -0,0 +1,9 @@
+package primitive_test
+
+import (
+	"testing"
+)
+
+func TestList(t *testing.T) {
+	testFromFile(t, "testdata/list.txt")
+}
---
+++ pkg/interpreter/primitive/obj_primitive_tuple_test.go
@@ -0,0 +1,9 @@
+package primitive_test
+
+import (
+	"testing"
+)
+
+func TestTuple(t *testing.T) {
+	testFromFile(t, "testdata/tuple.txt")
+}
